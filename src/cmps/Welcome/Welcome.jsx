0689326b81d7b242d74d94a97b17911cfef5f875
mazen-elbeik <mzmz.mesoverse@gmail.com>
2023-05-08 16:55:57 +0300

    modal

--- src/cmps/Welcome/Welcome.jsx
+++ src/cmps/Welcome/Welcome.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import CenterModal from '../CentralModal/CenterModal';
+import { useState } from 'react';
 const Welcome = ()=> {
+    const user = useSelector((state) => state.user.email);
+    const [showIntro, setShowIntro] = useState(false);
+    const handleCloseIntro = () => setShowIntro(false);
+    const handleShowIntro = () => setShowIntro(true);
     return (
     <div className="WelcomePage">
         <div className="WelcomePage-content">
-            <img src="/imgs/IoT.png" alt="Logo" style={{height:'30vh', transform:'translate(7px, 0)'}}/>
-            <h1 className='WelcomeMsg'>Welcome to MZmZ IoT</h1>
+            <img src="/imgs/LOGO3trans.png" alt="Logo" style={{height:'30vh', transform:'translate(7px, 0)'}}/>
+            <h1 className='WelcomeMsg'>{`Welcome ${user}`}</h1>
             <p>Connect devices through internet</p>
-            <Button as={Link} to={'/cubesurfer'} variant='primary' className='button1 button2'>Play Game</Button>
-            <Button as={Link} to={'/workouts'} variant='secondary' className='button1 button2'>Intro</Button>
+            <Button variant='primary' className='button1 button2' onClick={handleShowIntro}>Game Intro</Button>
+            <Button as={Link} to={'/cubesurfer'} variant='secondary' className='button1 button2'>Play Demo</Button>
+            <CenterModal
+                show={showIntro}
+                title="CubeSurfer Intro"
+                intropath='/vids/IntroTrim.mp4'
+                onHide={() => handleCloseIntro()}
+            />
         </div>
     </div>
     );
