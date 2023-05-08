1e6222cfdc74beb397e5d6f769b8d5b5330f411b
mazen-elbeik <mzmz.mesoverse@gmail.com>
2023-05-08 12:55:01 +0300

    update aspect

--- src/cmps/Game/Game.jsx
+++ src/cmps/Game/Game.jsx
@@ -1,5 +1,6 @@
 import { Unity, useUnityContext } from "react-unity-webgl";
-
+import { useState } from "react";
+import { useCallback } from "react";
 const Game = () => {
     const {unityProvider, loadingProgression, isLoaded} = useUnityContext({
         loaderUrl: "/cubesurfer/Build/cubesurfer.loader.js",
@@ -7,15 +8,43 @@ const Game = () => {
         frameworkUrl: "/cubesurfer/Build/cubesurfer.framework.js",
         codeUrl: "/cubesurfer/Build/cubesurfer.wasm",
     });
+
+    const [devicePixelRatio, setDevicePixelRatio] = useState(
+        window.devicePixelRatio
+      );
+    
+      const handleChangePixelRatio = useCallback(
+        function () {
+          // A function which will update the device pixel ratio of the Unity
+          // Application to match the device pixel ratio of the browser.
+          const updateDevicePixelRatio = function () {
+            setDevicePixelRatio(window.devicePixelRatio);
+          };
+          // A media matcher which watches for changes in the device pixel ratio.
+          const mediaMatcher = window.matchMedia(
+            `screen and (resolution: ${devicePixelRatio}dppx)`
+          );
+          // Adding an event listener to the media matcher which will update the
+          // device pixel ratio of the Unity Application when the device pixel
+          // ratio changes.
+          mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+          return function () {
+            // Removing the event listener when the component unmounts.
+            mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+          };
+        },
+        [devicePixelRatio]
+      );
+
     return ( 
-        <>
+        <div style={{textAlign:"center"}}>
             <h1>GAME SHOULD BE LOADED HERE</h1>
             {!isLoaded && (
                 <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
             )}
-            <Unity style={{width:"100%"}} unityProvider = {unityProvider}/>
+            <Unity style={{ width: '80%'}} devicePixelRatio={devicePixelRatio} unityProvider = {unityProvider}/>
             <h1>GAME SHOULD BE LOADED HERE</h1>
-        </>
+        </div>
      );
 }
  
