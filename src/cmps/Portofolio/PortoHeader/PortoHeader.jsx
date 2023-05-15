6f78a2854c2b02e94733c29bd3bcdd2d52fa6177
mazen-elbeik <mzmz.mesoverse@gmail.com>
2023-05-15 15:42:50 +0300

    porto

--- src/cmps/Portofolio/PortoHeader/PortoHeader.jsx
+++ src/cmps/Portofolio/PortoHeader/PortoHeader.jsx
@@ -1,12 +1,14 @@
+import { Image } from "react-bootstrap";
 import TimeLine from "./TimeLine";
-import { CircularProgress } from "@mui/material";
+import { CircularProgress, ImageListItemBar } from "@mui/material";
 const PortoHeader = () => {
     return ( 
         <>
             <div className="header" >
                 <p>PORTFOLIO IS UNDER CONSTRUCTION</p><CircularProgress />
                 <br />
-                <img className="avatar" src="/imgs/prof.jpg"/>
+                <Image className="avatar" src="/imgs/prof.jpg" roundedCircle/>
+                <br />
                 <div className="title">
                     <h1 className="name">Mazen Elbeik</h1> <br/> <p className="profeciency"><strong>MERN STACK DEVELOPER & IoT ENGINEER</strong></p>
                 </div>
--- src/cmps/Welcome/Welcome.jsx
+++ src/cmps/Welcome/Welcome.jsx
@@ -14,17 +14,11 @@ const Welcome = ()=> {
     return (
     <div className="WelcomePage">
         <div className="WelcomePage-content">
-                
-            <Row>
-                <Col xs={1} md={1} lg={1} style={{margin: "auto"}}>
-                        <TimeLine/>
-                </Col>
-                    <Col style={{textAlign:"center", transform:"translate(-30px,0px)"}}>
+  
+            
                         <img src='/imgs/prof.jpg' style={{width: 200, height: 200,borderRadius:"100px",display:"inline-block"}}/>
                         <br/>
                         <img src="/imgs/LOGO3trans.png" alt="Logo" style={{height:'30vh', transform:'translate(7px, 0)'}}/>
-                    </Col>
-                </Row>
                     <h1 className='WelcomeMsg'>{`Welcome ${user.split("@")[0]}`}</h1>
                     <p>LETS ORBIT THE GALAXY</p>
                     <Button variant='primary' className='button1 button2' onClick={handleShowIntro}>Game Intro</Button>
